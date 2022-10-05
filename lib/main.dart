05343fe8744a1ca8af90b509bae759deafa3081d
HaldunMatar <kh82matar@gmail.com>
2022-10-05 09:07:02 +0300

    staleless stateull

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/question.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+
+  answerQuestion() {
+    setState(() {
+      print(' I am answer  $questionIndex ');
+      questionIndex = questionIndex + 1;
+    });
+
+    // questionIndex +=1 ;
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['Q1 2*5', 'Q2 25 /  5 ', 'Q3  4 * 9 ', 'Q4  8 * 7 '];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz app'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text('2*15'),
-            SizedBox(
-              height: 20,
+            Question(questions[questionIndex]),
+            ElevatedButton(
+              child: Text('answer'),
+              onPressed: answerQuestion,
             ),
             ElevatedButton(
-              child: Text('31'),
-              onPressed: (() {
-                print('answer1');
-              }),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 10,
+              child: Text('answer'),
+              onPressed: answerQuestion,
             ),
             ElevatedButton(
-              child: Text('30'),
-              onPressed: (() {
-                print('answer2');
-              }),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              child: Text('32'),
-              onPressed: (() {
-                print('answer3');
-              }),
+              child: Text('answer'),
+              onPressed: answerQuestion,
             ),
           ],
         ),
